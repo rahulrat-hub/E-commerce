2b40db6f643aaf3d7219a6f55232b76868f056cd
Rahul Raturi <rahulraturi84@gmail.com>
2026-04-05 20:07:30 +0530

    contact page add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
 
 
 {/* {Flex} */}
-<div className="flex max-w-4xl px-5 gap-10 my-8">
+<div className="flex max-w-5xl px-5 gap-10 my-8 ml-20">
 
 {/* {LEFT INFO} */}
 <motion.div className="">
@@ -29,31 +29,31 @@ function Contact() {
 
 {/* {RIGHT INFO} */}
 
-<motion.form className='flex flex-col gap-4 border border-white'>
+<motion.form className='flex flex-col gap-10 py-10 px-8 border border-white w-full h-full rounded-xl'>
 
 {/* {NAME} */}
 <input type="text"
 placeholder='Your Name'
-className='text-white' 
+className='text-white  border border-[#2A2A2A] py-2 px-2 rounded-[10px] hover:border-white transition-all duration-300 ease-in-out' 
 />
 
 {/* {EMAIL} */}
 
 <input type="text" 
 placeholder='Enter E-mail'
-className='text-white'
+className='text-white  border border-[#2A2A2A] py-2 px-2 rounded-[10px] hover:border-white transition-all duration-300 ease-in-out'
 />
 
 {/* {FEEDBACK} */}
 
 <input type="text" 
 placeholder='Enter Your FeedBack'
-className='text-white'
+className='text-white  border border-[#2A2A2A] py-8 px-2  rounded-[10px] focus:bg-white transition-color duration-300 ease-in'
 />
 
 {/* {BUTTON} */}
 
-<button className='text-white'>Send Message</button>
+<button className='text-black bg-white hover:bg-gray-200 py-2 px-2 rounded-[10px] transition cursor-pointer'>Send Message</button>
 </motion.form>
 
 
